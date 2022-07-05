73792344dd5836800b6ff4c11ac947cbdaf6e2d0
D0N-B0T <salinasjoost@gmail.com>
2022-07-05 12:40:33 -0400

    changed api

--- bot.py
+++ bot.py
@@ -29,23 +29,11 @@ def getdate():
 
 #USDCLP
 def getusd():
-    url = 'https://www.xe.com/es/currencyconverter/convert/?Amount=1&From=USD&To=CLP'
-    page = requests.get(url)
-    soup = BeautifulSoup (page.content , 'html.parser')
-    algo = soup.find_all('p' , class_='result__BigRate-sc-1bsijpp-1 iGrAod')
-    valor = list()
-    for i in algo:
-        valor.append(i.text)
-
-    separado = valor.pop(0)
-    separado=separado.split()
-    separado=separado.pop(0)
-    separado=list(separado)
-    separado[3]="."
-    separado=''.join(separado)
-    separado=int(float(separado))
-    separado=round(separado)
-    return separado
+    web = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=usd&tsyms=clp"
+    response = requests.get(web)
+    data = response.json()
+    price = data["RAW"]["USD"]["CLP"]["PRICE"]
+    return price
 
 #BTCUSD
 def getbtcusd():
